b84ebc471d6c8b8eff3c16b8d18307e7ad1ae920
Alvin Panerio <panerio.alvin18@gmail.com>
2022-09-28 19:59:50 +0800

    Adding fight scene

--- fighting-game/Assets/Scripts/FightSceneGameHandler.cs
+++ fighting-game/Assets/Scripts/FightSceneGameHandler.cs
@@ -4,7 +4,41 @@ using UnityEngine;
 
 public class FightSceneGameHandler : MonoBehaviour
 {
-    void Start() { }
+    public GameObject p1HealthText;
+    public GameObject p2HealthText;
+    public GameObject p1Name;
+    public GameObject p2Name;
 
-    void Update() { }
+    void Start()
+    {
+        setHealth();
+        getPlayerName();
+    }
+
+    void Update()
+    {
+        changeHealthText();
+    }
+
+    void getPlayerName()
+    {
+        p1Name.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text =
+            PlayerScript.p1Name.ToString();
+        p2Name.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text =
+            PlayerScript.p2Name.ToString();
+    }
+
+    void setHealth()
+    {
+        PlayerScript.p1Health = PlayerScript.gameHealth;
+        PlayerScript.p2Health = PlayerScript.gameHealth;
+    }
+
+    void changeHealthText()
+    {
+        p1HealthText.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text =
+            PlayerScript.p2Health.ToString();
+        p2HealthText.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text =
+            PlayerScript.p1Health.ToString();
+    }
 }
